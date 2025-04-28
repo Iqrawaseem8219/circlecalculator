582f500a3b70c33521f024e3ae55a9d4371cba3b
Iqrawaseem8219 <iqrawaseem674@gmail.com>
2025-04-28 01:22:05 -0700

    Add files via upload

---
+++ index.js
@@ -0,0 +1,8 @@
+
+let radius = parseFloat(prompt("Enter the radius of the circle:"));
+
+let diameter = 2 * radius;
+let circumference = 2 * 3.1414 * radius;
+let area = 3.1414* radius * radius;
+
+alert("Diameter: " + diameter + "\nCircumference: " + circumference + "\nArea: " + area);
